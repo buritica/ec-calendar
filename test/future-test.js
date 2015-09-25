2c134cc1da095478df3badaaa9d061a780f32312
sescobb27 <s.escob.b.27@gmail.com>
2015-09-25 13:40:35 -0500

    [fix] close #1

---
+++ test/future-test.js
@@ -0,0 +1,44 @@
+import EcCalendar from '../lib/';
+import chai from 'chai';
+import sinon from 'sinon';
+
+var expect = chai.expect;
+describe('Future change seasons year', function () {
+  context('winter 2016', function () {
+    var clock;
+    beforeEach(function () {
+      clock = sinon.useFakeTimers(new Date(2016, 0, 1).getTime());
+      var calendar = new EcCalendar('WN16');
+      this.upcomingSeason = calendar.upcomingSeason;
+    });
+
+    afterEach(function () {
+      clock.restore();
+    });
+
+    it('next season should be spring and should be charged this year 2016', function () {
+      expect(this.upcomingSeason.id).to.equal('SP16');
+      expect(this.upcomingSeason.dates.charge.getFullYear()).to.equal(2015);
+      expect(this.upcomingSeason.dates.charge.getMonth()).to.equal(11);
+    });
+  });
+
+  context('winter 2017', function () {
+    var clock;
+    beforeEach(function () {
+      clock = sinon.useFakeTimers(new Date(2017, 0, 1).getTime());
+      var calendar = new EcCalendar('WN17');
+      this.upcomingSeason = calendar.upcomingSeason;
+    });
+
+    afterEach(function () {
+      clock.restore();
+    });
+
+    it('next season should be spring and should be charged this year 2016', function () {
+      expect(this.upcomingSeason.id).to.equal('SP17');
+      expect(this.upcomingSeason.dates.charge.getFullYear()).to.equal(2016);
+      expect(this.upcomingSeason.dates.charge.getMonth()).to.equal(11);
+    });
+  });
+});
