d9110aba23ccec43e1e2392e01f507f5f218f532
Nicolle Jimenez <nicolle.jimenez91@gmail.com>
2017-11-15 17:01:12 -0500

    [dist] broccoli build, update readme and format index with ;

--- dist/amd/index.js
+++ dist/amd/index.js
@@ -10,55 +10,78 @@ define(
     /**
      * Locals
      */
-    var today = new Date();
-    var chargeDay = 15;
-    var shipDay = 1;
-    var inStoreDay = 1;
-
+    const today = new Date();
+    const chargeDay = 15;
+    const shipDay = 1;
+    const inStoreDay = 1;
 
     /**
      * Years
      */
-    var firstYear = 2011;
-    var currentYear = today.getFullYear();
-    var nextTwoYears = currentYear + 2;
-    var years = [];
+    const firstYear = 2011;
+    const currentYear = today.getFullYear();
+    const nextTwoYears = currentYear + 2;
+    const years = [];
 
-    for (var i = nextTwoYears - firstYear; i >= 0; i--) {
+    for (let i = nextTwoYears - firstYear; i >= 0; i--) {
       years.push(nextTwoYears - i);
     }
 
     /**
      * Season Defaults
      */
-    var winter = {
+    const winter = {
+      type: 'shirt',
       code: 'WN',
       title: 'Winter',
       chargeMonth: 8,
       shipMonth: 11,
-      inStoreMonth: 7,
+      inStoreMonth: 6,
       kind: 'winter'
     };
 
-    var spring = {
+    const suitWinter = {
+      type: 'suit',
+      code: 'WN',
+      title: 'Winter',
+      chargeMonth: 8,
+      shipMonth: 11,
+      inStoreMonth: 3,
+      kind: 'winter'
+    };
+
+    const spring = {
+      type: 'shirt',
       code: 'SP',
       title: 'Spring',
       chargeMonth: 11,
       shipMonth: 2,
-      inStoreMonth: 10,
+      inStoreMonth: 9,
       kind: 'spring'
     };
 
-    var summer = {
+    const summer = {
+      type: 'shirt',
       code: 'SM',
       title: 'Summer',
       chargeMonth: 2,
       shipMonth: 5,
-      inStoreMonth: 1,
+      inStoreMonth: 0,
       kind: 'summer'
     };
 
-    var fall = {
+    const suitSummer = {
+      type: 'suit',
+      code: 'SM',
+      title: 'Summer',
+      chargeMonth: 2,
+      shipMonth: 5,
+      inStoreMonth: 9,
+      kind: 'summer'
+    };
+
+    const fall = {
+      type: 'shirt',
       code: 'FA',
       title: 'Fall',
       chargeMonth: 5,
@@ -67,30 +90,38 @@ define(
       kind: 'fall'
     };
 
-    var defaults = [
+    const defaults = [
       spring,
       summer,
+      suitSummer,
       fall,
-      winter
+      winter,
+      suitWinter
     ];
 
-    function buildSeasons() {
+    function buildSeasons () {
       /**
        * Build all seasons for each available year
        */
 
-      var seasons = years.map(function (year) {
-        var suffix = year - 2000;
+      const seasons = years.map(function (year) {
+        const suffix = year - 2000;
 
         return defaults.map(function (season) {
-          var chargeYear = year;
-          var shipYear = year;
+          let inStoreYear = year;
+          let chargeYear = year;
+          const shipYear = year;
 
           if (season.code === 'SP') {
             chargeYear -= 1;
+            inStoreYear -= 1;
+          } else if (season.code === 'SM' && season.type === 'suit') {
+            inStoreYear -= 1;
           }
-          var inStoreDate = new Date(chargeYear, season.inStoreMonth, inStoreDay);
+          const inStoreDate = new Date(inStoreYear, season.inStoreMonth, inStoreDay);
+
           return {
+            type: season.type,
             id: season.code + suffix,
             code: season.code,
             title: season.title + ' ' + year,
@@ -102,7 +133,7 @@ define(
             startDate: inStoreDate,
             year: year,
             kind: season.kind
-          };
+          }
         });
       });
       return _.flatten(seasons);
@@ -111,19 +142,21 @@ define(
     /**
      * Calendar
      */
-    function Calendar(id) {
+    function Calendar (id) {
       this.years = years;
       this.seasons = buildSeasons();
+      this.shirtSeasons = this.seasons.filter(season => season.type === 'shirt');
+      this.suitSeasons = this.seasons.filter(season => season.type === 'suit');
 
-      this.activeShirtSeason = this.findSeason(id);
-      var activeIndex = this.seasons.indexOf(this.activeShirtSeason);
-      this.previousShirtSeason = this.seasons[activeIndex - 1];
-      this.upcomingShirtSeason = this.seasons[activeIndex + 1];
+      this.activeShirtSeason = this.findSeason(id, 'shirt');
+      var activeIndex = this.shirtSeasons.indexOf(this.activeShirtSeason);
+      this.previousShirtSeason = this.shirtSeasons[activeIndex - 1];
+      this.upcomingShirtSeason = this.shirtSeasons[activeIndex + 1];
 
       this.activeSuitSeason = this.findSuitSeason(id);
-      var activeSuitIndex = this.seasons.indexOf(this.activeSuitSeason);
-      this.previousSuitSeason = this.seasons[activeSuitIndex - 2];
-      this.upcomingSuitSeason = this.seasons[activeSuitIndex + 2];
+      var activeSuitIndex = this.suitSeasons.indexOf(this.activeSuitSeason);
+      this.previousSuitSeason = this.suitSeasons[activeSuitIndex - 1];
+      this.upcomingSuitSeason = this.suitSeasons[activeSuitIndex + 1];
     }
 
     /**
@@ -131,23 +164,23 @@ define(
      * @param  {String} id Season Code (e.g. WN11)
      * @return {Season}
      */
-    Calendar.prototype.findSeason = function (id) {
+    Calendar.prototype.findSeason = function (id, type = 'shirt') {
       if (!id) {
         throw Error('season id not specified');
       }
-      return _.find(this.seasons, { id: id });
+      return _.find(this.seasons, { id: id, type: type });
     }
 
     Calendar.prototype.findSuitSeason = function (seasonId) {
-      const season = seasonId.slice(0, 2)
-      const seasonYear = seasonId.slice(2)
+      const season = seasonId.slice(0, 2);
+      const seasonYear = seasonId.slice(2);
       switch (season) {
         case 'SP':
         case 'SM':
-          return this.findSeason(`SM${seasonYear}`)
+          return this.findSeason(`SM${seasonYear}`, 'suit');
         case 'FA':
         case 'WN':
-          return this.findSeason(`WN${seasonYear}`)
+          return this.findSeason(`WN${seasonYear}`, 'suit');
       }
     }
 
